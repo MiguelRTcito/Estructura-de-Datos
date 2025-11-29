3de79f39e7ed4720ce540ddddfd19631a24827df
MiguelRTcito <miguel.rdzt.06@gmail.com>
2025-11-29 16:18:29 -0600

    Inserccion

---
+++ Parcial 3/Inserccion/Inserccion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Inserccion
+{
+    public void Agregar(int[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            int llave = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > llave)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = llave;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese el tamaño del arreglo: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arreglo = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Ingrese el valor del elemento {i + 1}: ");
+                arreglo[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Inserccion insercion = new Inserccion();
+            int[] array = arreglo;
+            Console.WriteLine("Array original: " + string.Join(", ", array));
+            insercion.Agregar(array);
+            Console.WriteLine("Array ordenado: " + string.Join(", ", array));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Parcial 3/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+public class MergeSort
+{
+    public static void MergeSortArray(int[] array)
+    {
+        if (array.Length <= 1)
+            return;
+
+        int mid = array.Length / 2;
+
+        int[] left = new int[mid];
+        int[] right = new int[array.Length - mid];
+
+        // Llenar left
+        for (int i = 0; i < mid; i++)
+            left[i] = array[i];
+
+        // Llenar right
+        for (int j = mid; j < array.Length; j++)
+            right[j - mid] = array[j];
+
+        // Recursión
+        MergeSortArray(left);
+        MergeSortArray(right);
+
+        // Mezclar
+        int leftIndex = 0, rightIndex = 0, mergedIndex = 0;
+
+        // Mientras haya elementos en ambos subarreglos
+        while (leftIndex < left.Length && rightIndex < right.Length)
+        {
+            if (left[leftIndex] <= right[rightIndex])
+            {
+                array[mergedIndex] = left[leftIndex];
+                leftIndex++;
+            }
+            else
+            {
+                array[mergedIndex] = right[rightIndex];
+                rightIndex++;
+            }
+
+            mergedIndex++;
+        }
+
+        // Copiar lo que queda en left (si queda)
+        while (leftIndex < left.Length)
+        {
+            array[mergedIndex] = left[leftIndex];
+            leftIndex++;
+            mergedIndex++;
+        }
+
+        // Copiar lo que queda en right (si queda)
+        while (rightIndex < right.Length)
+        {
+            array[mergedIndex] = right[rightIndex];
+            rightIndex++;
+            mergedIndex++;
+        }
+    }
+public static void Main(string[] args)
+{
+    Console.Write("¿Cuántos valores deseas ingresar? ");
+    int n = int.Parse(Console.ReadLine());
+
+    int[] array = new int[n];
+
+    Console.WriteLine("\nIngresa los valores:");
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Valor {i + 1}: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine("\nOrdenando el arreglo con MergeSort...\n");
+
+    MergeSort.MergeSortArray(array);
+
+    Console.WriteLine("Arreglo ordenado:");
+    Console.WriteLine(string.Join(", ", array));
+
+    Console.ReadKey();
+}
+
+
+}
